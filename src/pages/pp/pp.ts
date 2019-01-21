362ffb9f9d8e2ff9fa0587e4971ac2bccafbc010
CHAPEAU Nicolas <chapenicolas@gmail.com>
2019-01-21 18:49:10 +0100

    update page html basic ts

--- src/pages/pp/pp.ts
+++ src/pages/pp/pp.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 import {Http} from "@angular/http";
 import 'rxjs/add/operator/map';
 
@@ -16,32 +15,26 @@ import 'rxjs/add/operator/map';
   selector: 'page-pp',
   templateUrl: 'pp.html',
 })
+
 export class PpPage {
 
 
   url:string;
-  data:string;
+  data:object;
 
   constructor(public http: Http, public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PpPage');
-    this.loadData();
+  ionViewWillLoad() {
+      this.http.get('https://nicolas.okbutwin.fr/myplanner/api/?prestations=all')
+          //.map(res => res.json())
+          .subscribe(data => {
+
+              this.data = res.json();
+          },err => {console.log(err);
+
+          });
   }
-
-  loadData(){
-
-    this.http.get('https://nicolas.okbutwin.fr/myplanner/api/?products=all')
-        .map(res => res.json())
-        .subscribe(data => {
-            this.data = data;
-            console.log(data);
-        },err => {console.log(err);
-
-        });
-
-}
 
 }
 
