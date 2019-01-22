b54bab3f380ed107316b746ebd59ec43f2bb6a6f
ThomasBONNELYE <thomas.bonnelye@devinci.fr>
2019-01-23 00:48:18 +0100

    Home Product

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FilterPage } from '../filter/filter';
+import { NavParams } from 'ionic-angular';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'page-home',
@@ -8,10 +12,54 @@ import { FilterPage } from '../filter/filter';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  famous: any = [];
+  new: any = [];
+  url: string;
 
-  }
-  goFilter() {
-    this.navCtrl.push(FilterPage, {}, { animate: true, direction: 'forward' });
+constructor(public http: Http, public navCtrl: NavController, public navParams: NavParams) {
 }
+
+ionViewWillLoad() {
+
+    this.http.get('https://nicolas.okbutwin.fr/myplanner/api/?products=all')
+        .map(res => res.json())
+        .subscribe(data => {
+
+            this.famous.push(data.data[0]);
+            this.famous.push(data.data[1]);
+            this.famous.push(data.data[2]);
+            this.new.push(data.data[3]);
+            this.new.push(data.data[4]);
+            this.new.push(data.data[5]);
+
+        },err => {console.log(err);
+    });
 }
+
+goFilter() {
+  this.navCtrl.push(FilterPage, {}, { animate: true, direction: 'forward' });
+}
+
+loadData(type) {
+
+        this.http.get('https://nicolas.okbutwin.fr/myplanner/api/?products=all')
+            .map(res => res.json())
+            .subscribe(data => {
+
+
+                this.famous = [];
+                this.new = [];
+
+                this.famous.push(data.data[0]);
+                this.famous.push(data.data[1]);
+                this.famous.push(data.data[2]);
+                this.new.push(data.data[3]);
+                this.new.push(data.data[4]);
+                this.new.push(data.data[5]);
+
+            },err => {console.log(err);
+
+            });  
+    }
+}
+
