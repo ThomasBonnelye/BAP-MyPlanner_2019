af81fc74cb265661c0594ded757a756a96683b21
ThomasBONNELYE <thomas.bonnelye@devinci.fr>
2019-01-25 18:55:11 +0100

    USerLogo fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { MenuController } from 'ionic-angular';
 
 import { LoginPage } from '../pages/login/login';
 import { TabsPage } from '../pages/tabs/tabs';
+
+import { NativeStorage } from '@ionic-native/native-storage';
 import { Storage } from '@ionic/storage';
 
 @Component({
@@ -13,11 +15,15 @@ import { Storage } from '@ionic/storage';
 })
 export class MyApp {
 
+  public username: string;
+  public userlogo: string;
+  public userperm: string;
+
   @ViewChild(Nav) nav: Nav;
 
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public menuCtrl: MenuController, public storage: Storage) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public menuCtrl: MenuController, public storage: Storage, private nativeStorage: NativeStorage) {
     platform.ready().then(() => {
       this.storage.clear().then(() => {
        console.log('all keys cleared');
@@ -30,6 +36,17 @@ export class MyApp {
   onClick() {
     this.menuCtrl.close();
     this.nav.push(LoginPage);
+  }
+
+  menuOpened() {
+    this.nativeStorage.getItem('userdata').then((data)=>{
+      this.username = data.username;
+      this.userlogo = '../../assets/imgs/users/' + data.username + '.png';
+      this.userperm = data.perm;
+      console.log('Le nom est:', this.username);
+      console.log('Le chemin de limage est:', this.userlogo);
+      console.log('LUser perm est:', this.userperm);
+  }); 
   }
 
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -7,7 +7,7 @@ import { PpPage } from '../pp/pp';
 import { AccountPage } from '../account/account';
 import { PanierPage } from '../panier/panier';
 
-import { NativeStorage } from '@ionic-native/native-storage';
+// import { NativeStorage } from '@ionic-native/native-storage';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -22,48 +22,19 @@ export class TabsPage {
 
   PanierButton: any;
 
-  public username: string;
-  public userlogo: string;
-  public userperm: string;
+//   public username: string;
+//   public userlogo: string;
+//   public userperm: string;
 
-  constructor(public navController: NavController, private nativeStorage: NativeStorage) {
+  constructor(public navController: NavController/*, private nativeStorage: NativeStorage*/) {
       this.PanierButton = PanierPage;
   }
 
-  logo() {
-    this.nativeStorage.getItem('userdata').then((data)=>{
-      this.username = data.username;
-      this.userlogo = '../../assets/imgs/users/' + data.username + '.png';
-      this.userperm = data.perm;
-    });
-  }
-    /*
-        setValue() {
-            this.nativeStorage.setItem('userterms', {terms: 1})
-        }
-
-        ionViewWillEnter() {
-            this.nativeStorage.getItem('userterms').then((data)=>{
-                this.terms = data.terms;
-                this.popUp();
-            });
-        }
-
-        popUp() {
-            if (this.terms != '1') {
-                let alert = this.alertCtrl.create({
-                    title: 'Collecte des données',
-                    message: 'Vous devez autoriser MyPlanner à récolter des données pour garantir son fonctionnement normal.',
-                    buttons: [
-                        {
-                            text: 'Accepter',
-                            handler: () => {
-                                this.setValue();
-                            }
-                        }
-                    ]
-                });
-                alert.present();
-            }
-        }*/
+//   logo() {
+//     this.nativeStorage.getItem('userdata').then((data)=>{
+//       this.username = data.username;
+//       this.userlogo = '../../assets/imgs/users/' + data.username + '.png';
+//       this.userperm = data.perm;
+//     });
+//   }
 }
